c9b2e4a710e057b0384fc80e03d06454eddea8d6
Shane Soderstrom <shanesody@outlook.com>
2019-09-23 23:08:51 -0600

    Added logic and psudo code for when their is only a captain and coins.

--- PresentationForm/PresentationForm/PirateBooty.cs
+++ PresentationForm/PresentationForm/PirateBooty.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace PresentationForm
 {
     class PirateBooty
@@ -23,11 +24,39 @@ namespace PresentationForm
         {
             if (NumberOfCoins == 0 || NumberOfPirates == 0)
             {
-                
+                CaptainEarnings = 0;
+                FirstMateEarnings = 0;
+                ShipEarnings = 0;
+                PirateEarnings = 0;
+                NumberOfCoins = 0;
+                NumberOfPirates = 0;
+
+                if (NumberOfCoins != 0)
+                {
+                    if (NumberOfCoins == 1)
+                    {
+                        ShipEarnings = NumberOfCoins;
+                    }
+                    else
+                    {
+                        double evens = NumberOfCoins % 2;
+
+                        double capPercent = CAPTAIN_PERCENT * NumberOfCoins;
+                        double shipPercent = SHIP_PERCENT * NumberOfCoins;
+                        if (true)
+                        {
+                            //CaptainEarnings = Math.Round(capPercent);
+                            //Round ship earnings
+                            //compare against gold cost to see if it equal
+                            //Divy difference
+                        }
+                    }
+                }
             }
             else
             {
                 //captain = 4 + 2 from bonus, ship = 6, numPirates = 6, firstmate bonus = 2, numCoins = 20 CoinPerPirate = 1
+
             }
         }
     }
